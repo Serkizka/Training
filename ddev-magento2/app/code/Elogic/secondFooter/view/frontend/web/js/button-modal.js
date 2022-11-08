5cb7d4fbfb1e1b9d6fa880abb7ac3f483075b7a8
Serkizka <anastasiaserkiz@gmail.com>
2022-11-09 01:16:26 +0200

    Change text and function

--- ddev-magento2/app/code/Elogic/secondFooter/view/frontend/web/js/button-modal.js
+++ ddev-magento2/app/code/Elogic/secondFooter/view/frontend/web/js/button-modal.js
@@ -20,40 +20,49 @@ function close() {
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 let guessField = document.querySelector('.guessField');
 let userGuess = Number(guessField.value);
+let buttonSubmit = document.querySelector('.buttonSubmit');
+let falseInput = document.querySelector('.falseInput');
 let guesses = document.querySelector('.guesses');
 let lastResult = document.querySelector('.lastResult');
+
 let lowOrHi = document.querySelector('.lowOrHi');
-let guessSubmit = document.querySelector('.guessSubmit');
 let guessCount = 1;
-guessField.focus();
 
 function checkGuess() {
 
-    if (guessCount === 1 || guessCount > 1) {
-        guesses.textContent = 'Last guesses: ';
+    if (guessCount < 1 || guessCount > 50) {
+        falseInput.textContent = 'Write a number from 1 to 50';
+    }
+
+    // if (guessCount === 1 || guessCount > 1) {
+    //     guesses.textContent = 'Last guesses: ';
+    // }
+    // guesses.textContent += userGuess + ' ';
+
+    if (userGuess === randomNumber) {
+            guesses.textContent = 'Previous guesses: ';
+        alert(randomNumber);
     }
     guesses.textContent += userGuess + ' ';
 
-    if (userGuess === randomNumber) {
-        guesses.textContent = 'Last guesses: ';
-        alert(randomNumber);
-    } else {
-        lastResult.textContent = "Wrong!";
+    if (userGuess > randomNumber) {
+        lastResult.textContent = "The number entered was greater than the random number";
+    } else if (userGuess > randomNumber) {
+        lastResult.textContent = "The number entered was below the random number";
         alert(randomNumber);
     }
     guessField.value = '';
 }
 
-guessSubmit.addEventListener('click', checkGuess);
+buttonSubmit.addEventListener('click', checkGuess);
 
 
-function setGameOver() {
-    guessField.disabled = true;
-    guessSubmit.disabled = true;
-    resetButton = document.createElement('button');
-    resetButton.textContent = 'Play Again?';
-    document.body.appendChild(resetButton);
-    resetButton.addEventListener('click', resetGame);
-}
+// function setGameOver() {
+//     buttonSubmit.disabled = true;
+//     resetButton = document.createElement('button');
+//     resetButton.textContent = 'Play Again?';
+//     document.body.appendChild(resetButton);
+//     resetButton.addEventListener('click', resetGame);
+// }
 
 
