e1485adb716d03e87dfb4140f5a293cbaaeaf287
Serkizka <anastasiaserkiz@gmail.com>
2022-11-10 00:03:06 +0200

    add elements/styles for jquery

--- ddev-magento2/app/code/Elogic/secondFooter/view/frontend/web/js/jquery-modal.js
+++ ddev-magento2/app/code/Elogic/secondFooter/view/frontend/web/js/jquery-modal.js
@@ -10,15 +10,39 @@ define(["jquery"], function ($) {
     })
 
     const randomNumber = Math.ceil(Math.random() * 50);
-
+    // let guesses = (".guessField").val();
+    let guessess_p = [];
     $(".buttonSubmit").click(function (){
-        $(".guessField").val("");
 
-        $(".guessField").each(function (){
-            if($(this).val() < randomNumber) {
-                $(".rightAnswer").html('red');
+
+        alert(randomNumber)
+             guessess_p += " " +$(".guessField").val();
+            if($(".guessField").val() > randomNumber) {
+                $(".lastResult").html('The number entered was greater than the random number');
+            } else if($(".guessField").val() < randomNumber) {
+                $(".lastResult").html('The number entered was below the random number');
+            } else if($(".guessField").val() == randomNumber) {
+                // create an array
+                let guessess_array = guessess_p.split(' ');
+
+                // get last element of array
+                let lastEl = guessess_array.slice(-1);
+
+                // add span to el of array
+                let lastElWithSpan = "<span class='pink'>" + lastEl + "</span>";
+
+                guessess_array[guessess_array.length-1] = lastElWithSpan;
+                console.log(guessess_array);
+
+
+                // // add background-color for last element
+                // let $lastEl = $(lastEl);
+                // let addBackgroundColor = $lastEl.css("background-color", "pink");
+                //$(".rightAnswer").text("Previous guesses:" + guessess_array.join(' '));
+                $(".rightAnswer").html("Previous guesses:" + guessess_array.join(' '))
+                $(".lastResult").css("display", "none");
             }
-        })
+        $(".guessField").val("");
     })
 
 });
