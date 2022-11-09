7ae327e6fe03c84d2075edb468e2a5eb7a4ff69d
Serkizka <anastasiaserkiz@gmail.com>
2022-11-09 13:43:39 +0200

    split js and jquery, edit xml files

---
+++ ddev-magento2/app/code/Elogic/secondFooter/view/frontend/web/js/js-modal.js
@@ -0,0 +1,59 @@
+let modal = document.getElementById("modal");
+let btn = document.getElementById("footer-button");
+let overlay = document.getElementById("overlay");
+let closeBtn = document.getElementById("modal-close");
+
+
+btn.addEventListener("click", clicked);
+closeBtn.addEventListener("click", close);
+
+function clicked(ev) {
+    overlay.classList.toggle("active");
+    modal.classList.toggle("active");
+}
+
+function close() {
+    overlay.classList.toggle("active");
+    modal.classList.toggle("active");
+}
+
+let randomNumber = Math.ceil(Math.random() * 50);
+let guessField = document.querySelector('.guessField');
+let buttonSubmit = document.querySelector('.buttonSubmit');
+let rightAnswer = document.querySelector('rightAnswer');
+let lastResult = document.querySelector('.lastResult');
+
+alert(randomNumber);
+
+
+function checkGuess() {
+    let userGuess = Number(guessField.value);
+    // if (guessCount === 1 || guessCount > 1) {
+    //     guesses.textContent = 'Last guesses: ';
+    // }
+    // guesses.textContent += userGuess + ' ';
+
+    if (randomNumber === userGuess) {
+        alert('correct answer');
+    }
+
+    if (userGuess > randomNumber) {
+        lastResult.textContent = "The number entered was greater than the random number";
+    } else if (userGuess < randomNumber) {
+        lastResult.textContent = "The number entered was below the random number";
+    }
+    guessField.value = '';
+}
+
+buttonSubmit.addEventListener('click', checkGuess);
+
+
+// function setGameOver() {
+//     buttonSubmit.disabled = true;
+//     resetButton = document.createElement('button');
+//     resetButton.textContent = 'Play Again?';
+//     document.body.appendChild(resetButton);
+//     resetButton.addEventListener('click', resetGame);
+// }
+
+
